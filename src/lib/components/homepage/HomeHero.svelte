0b44651b65e5370f094cb9aec5646a8b08dbf601
baileyday <bailey.j.day@gmail.com>
2024-04-29 21:19:58 -0400

    fonts'

--- src/lib/components/homepage/HomeHero.svelte
+++ src/lib/components/homepage/HomeHero.svelte
@@ -2,11 +2,11 @@
 	<div class="container px-4 md:px-6">
 		<div class="mx-auto max-w-3xl text-center">
 			<h1
-				class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-7xl font-['Open_Sans']"
+				class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-7xl font-sans"
 			>
 				Transparency at Your Fingertips
 			</h1>
-			<p class="mt-6 text-lg text-gray-600 md:text-xl font-['Open_Sans']">
+			<p class="mt-6 text-lg text-gray-600 md:text-xl font-sans">
 				Access comprehensive, clear information about your elected officials. Empower your decisions
 				and foster accountability in politics, right from your local area to the federal level.
 			</p>
